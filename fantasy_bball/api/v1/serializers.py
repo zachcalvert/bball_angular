3c22893415ad618513aa9acdcde8e295c4d09562
Zach Calvert <zachc@darkhorse.com>
2016-10-19 12:52:45 -0700

    update team and player serializers

--- fantasy_bball/api/v1/serializers.py
+++ fantasy_bball/api/v1/serializers.py
@@ -1,12 +1,14 @@
-from rest_framework.serializers import ModelSerializer  
+from rest_framework.serializers import ModelSerializer, ReadOnlyField  
 from leagues.models import League, Team
 from players.models import Player
 
 
 class LeagueSerializer(ModelSerializer):
+	teams = ReadOnlyField(source='get_teams')
 
-    class Meta:
-        model = League
+	class Meta:
+		model = League
+		fields = ('id', 'name', 'manager', 'is_public', 'teams')
 
 class TeamSerializer(ModelSerializer):
 
--- fantasy_bball/api/v1/viewsets.py
+++ fantasy_bball/api/v1/viewsets.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets 
+from rest_framework import viewsets
  
 from leagues.models import League, Team
 from players.models import Player
@@ -18,3 +18,23 @@ class TeamViewSet(viewsets.ModelViewSet):
 class PlayerViewSet(viewsets.ModelViewSet):  
     queryset = Player.objects.all()
     serializer_class = serializers.PlayerSerializer
+
+    def get_queryset(self):
+    	queryset = Player.objects.all()
+    	league_id = self.request.query_params.get('league_id', None)
+    	free_agents = self.request.query_params.get('free_agents', None)
+    	position = self.request.query_params.get('pos', None)
+    	team = self.request.query_params.get('team', None)
+
+    	if league_id:
+    		if free_agents:
+	    		fa_ids = [player.id for player in queryset if player.is_available(league_id)]
+	    		queryset = queryset.filter(id__in=fa_ids)
+
+	    	if position:
+	    		queryset = queryset.filter(position=position)
+
+	    	if team:
+	    		queryset = queryset.filter(nba_team=team)
+
+    	return queryset
