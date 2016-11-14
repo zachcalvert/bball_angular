c5d72c47317c323d696286c2112dc61f7f7a99a2
Zach Calvert <zachc@darkhorse.com>
2016-11-13 18:01:16 -0800

    remove v1 dependencies, get_2017_bocscores

--- fantasy_bball/fantasy_bball/urls.py
+++ fantasy_bball/fantasy_bball/urls.py
@@ -2,13 +2,8 @@ from django.contrib import admin
 from django.conf.urls import include, url  
 from django.views.generic import TemplateView
 
-from api.v1.routes import api_router
-
 urlpatterns = [  
     url(r'^admin/', admin.site.urls),
-
-    # API:V1
-    url(r'^api/v1/', include(api_router.urls)),
 
     url(r'^api/v2/', include('leagues.api_urls')),
     # url(r'^api/v2/teams', include('teams.api_urls')),
--- fantasy_bball/schedule/management/commands/get_2017_boxscores.py
+++ fantasy_bball/schedule/management/commands/get_2017_boxscores.py
@@ -18,10 +18,10 @@ class Command(BaseCommand):
 		# can't get the boxscore for a game that hasn't been played
 		now = datetime.now()
 		today = now.date()
-		delta = timedelta(weeks=2)
-		last_week = today - delta
+		delta = timedelta(weeks=3)
+		three_weeks_ago = today - delta
 		# filter by which games aren't represented in the statlines table yet
-		games = Game.objects.filter(date__lt=today, date__gt=last_week)
+		games = Game.objects.filter(date__lt=today, date__gt=three_weeks_ago)
 
 		for game in games:
 			# don't add game stats twice
@@ -40,65 +40,52 @@ class Command(BaseCommand):
 
 			for row in away_rows:
 				name = row.find('th')
+
+				try:
+					player_name = name['csk']
+					last_name, first_name = player_name.split(',')
+					full_name = "{0} {1}".format(first_name, last_name)
+					player = Player.objects.get(name=full_name)
+				except Exception:
+					continue
+
 				try:
 					cells = row.findAll('td')
 				except Exception:
 					continue
 				for cell in cells:
-					if cell['data-stat'] == "box_score_text":
-						player_name = cell.text
-						try:
-							player = Player.objects.get(name=player_name)
-						except Player.DoesNotExist:
-							player=Player.objects.create(name=player_name)
-						if player.nba_team == 'FA':
-							player.nba_team = game.away_team
-							player.save()
-							print("added {0} to {1} roster".format(player.name, game.away_team))
-
-					if cell['data-stat'] == "box_score_text":
-
-					elif i == 1:
+					if cell['data-stat'] == "mp":
 						mp = cell.text
-					elif i == 2:
+					elif cell['data-stat'] == "fg":
 						fgm = cell.text
-					elif i == 3:
+					elif cell['data-stat'] == "fga":
 						fga = cell.text
-					elif i ==4:
-						pass
-					elif i == 5:
+					elif cell['data-stat'] == "fg3":
 						threesm = cell.text
-					elif i == 6:
+					elif cell['data-stat'] == "fg3a":
 						threesa = cell.text
-					elif i ==7:
-						pass
-					elif i == 8:
+					elif cell['data-stat'] == "ft":
 						ftm = cell.text
-					elif i == 9:
+					elif cell['data-stat'] == "fta":
 						fta = cell.text
-					elif i == 10:
-						pass
-					elif i == 11:
+					elif cell['data-stat'] == "orb":
 						orbs = cell.text
-					elif i == 12:
+					elif cell['data-stat'] == "drb":
 						drbs = cell.text
-					elif i == 13:
+					elif cell['data-stat'] == "trb":
 						trbs = cell.text
-					elif i == 14:
+					elif cell['data-stat'] == "ast":
 						asts = cell.text
-					elif i == 15:
+					elif cell['data-stat'] == "stl":
 						stls = cell.text
-					elif i == 16:
+					elif cell['data-stat'] == "blk":
 						blks = cell.text
-					elif i == 17:
+					elif cell['data-stat'] == "tov":
 						tos = cell.text
-					elif i == 18:
+					elif cell['data-stat'] == "pf":
 						pfs = cell.text
-					elif i == 19:
+					elif cell['data-stat'] == "pts":
 						pts = cell.text
-
-					i += 1
-
 
 				if row.find('td'):
 					if mp == 'Did Not Play' or mp == 'Player Suspended':
@@ -115,80 +102,71 @@ class Command(BaseCommand):
 						# TODO: update the Game instance with the final score
 						print('Loaded statline for {0} in game {1}'.format(player.name, game))
 
+
 			home_table = bs.find(lambda tag: tag.name=='table' and tag.has_attr('id') and tag['id']=="{}_basic".format(game.home_team))
 			try:
-				home_rows = home_table.findAll(lambda tag: tag.name=='tr')
+				home_rows = away_table.findAll(lambda tag: tag.name=='tr')
 			except Exception:
 				continue
 
 			for row in home_rows:
+				name = row.find('th')
+
+				try:
+					player_name = name['csk']
+					last_name, first_name = player_name.split(',')
+					full_name = "{0} {1}".format(first_name, last_name)
+					player = Player.objects.get(name=full_name)
+				except Exception:
+					continue
+
 				try:
 					cells = row.findAll('td')
 				except Exception:
 					continue
-				i = 0
 				for cell in cells:
-					if i == 0:
-						player_name = cell.text
-						try:
-							player = Player.objects.get(name=player_name)
-						except Player.DoesNotExist:
-							player = Player.objects.create(name=player_name)
-						if player.nba_team == 'FA':
-							player.nba_team = game.home_team
-							player.save()
-							print("added {0} to {1} roster".format(player.name, game.home_team))
-					elif i == 1:
+					if cell['data-stat'] == "mp":
 						mp = cell.text
-					elif i == 2:
+					elif cell['data-stat'] == "fg":
 						fgm = cell.text
-					elif i == 3:
+					elif cell['data-stat'] == "fga":
 						fga = cell.text
-					elif i ==4:
-						pass
-					elif i == 5:
+					elif cell['data-stat'] == "fg3":
 						threesm = cell.text
-					elif i == 6:
+					elif cell['data-stat'] == "fg3a":
 						threesa = cell.text
-					elif i ==7:
-						pass
-					elif i == 8:
+					elif cell['data-stat'] == "ft":
 						ftm = cell.text
-					elif i == 9:
+					elif cell['data-stat'] == "fta":
 						fta = cell.text
-					elif i == 10:
-						pass
-					elif i == 11:
+					elif cell['data-stat'] == "orb":
 						orbs = cell.text
-					elif i == 12:
+					elif cell['data-stat'] == "drb":
 						drbs = cell.text
-					elif i == 13:
+					elif cell['data-stat'] == "trb":
 						trbs = cell.text
-					elif i == 14:
+					elif cell['data-stat'] == "ast":
 						asts = cell.text
-					elif i == 15:
+					elif cell['data-stat'] == "stl":
 						stls = cell.text
-					elif i == 16:
+					elif cell['data-stat'] == "blk":
 						blks = cell.text
-					elif i == 17:
+					elif cell['data-stat'] == "tov":
 						tos = cell.text
-					elif i == 18:
+					elif cell['data-stat'] == "pf":
 						pfs = cell.text
-					elif i == 19:
+					elif cell['data-stat'] == "pts":
 						pts = cell.text
-
-					i += 1
-
 
 				if row.find('td'):
 					if mp == 'Did Not Play' or mp == 'Player Suspended':
 							continue
 					else:
+						# account for trades
 						if player.nba_team != game.home_team:
 							player.nba_team = game.home_team
 							player.save()
-
-						home_statline = StatLine.objects.create(game_id=game.id, player_id=player.id,
+						home_statline = StatLine.objects.create(game=game, player=player,
 							mp=mp, fgm=fgm, fga=fga,ftm=ftm, fta=fta, threesm=threesm, threesa=threesa,
 							orbs=orbs, drbs=drbs, trbs=trbs, asts=asts, stls=stls, blks=blks, tos=tos,
 							pfs=pfs,pts=pts)
